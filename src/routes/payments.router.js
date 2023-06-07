1463d55741474a7a9a54f885a91d09e00bedb65d
Facundo Sautu <fsautu@tecnoaccion.com.ar>
2023-06-07 11:37:20 -0300

    Agregado de politicas de acceso al endpoint de pagos

--- src/routes/payments.router.js
+++ src/routes/payments.router.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import PaymentService from "../services/payments.service.js";
+import { handlePolicies } from "../utils.js";
 
 const paymentsRouter = Router();
 
 const paymentService = new PaymentService();
 
-paymentsRouter.post('/payment-intent', async(req, res, next)=>{
+paymentsRouter.post('/payment-intent', handlePolicies(['USER', 'PREMIUM', 'ADMIN']), async(req, res, next)=>{
     try {
         const cartId = req.body.cartId;
     
